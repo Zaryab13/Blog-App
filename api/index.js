e7efc816a286ad4329177b508e28c6fc70d8b531
Zaryab Khan <zaryabcom123@gmail.com>
2024-01-27 23:29:13 +0500

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO;
+
+console.log(mongoURI)
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log("Mongodb is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('Server is Running on Port 3000!')
-}) 
+app.listen(3000, () => {
+  console.log("Server is Running on Port 3000!");
+});
